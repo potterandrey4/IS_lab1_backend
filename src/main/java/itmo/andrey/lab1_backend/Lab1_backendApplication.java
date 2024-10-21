e49bae3e58fdedec717d9c94fe20e343bee2aa46
andrey <ad061333@gmail.com>
2024-10-22 01:54:03 +0300

    init commit

---
+++ src/main/java/itmo/andrey/lab1_backend/Lab1_backendApplication.java
@@ -0,0 +1,13 @@
+package itmo.andrey.lab1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1_backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1_backendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itmo/andrey/lab1_backend/Laba1ApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.andrey.lab1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1_backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
